15a49657dcd4ab7fc0cd56320cc1fc50a94721aa
Eskombro <sjimenezre@gmail.com>
2019-01-27 13:51:57 +0100

    cleaning

--- src/handle_args.c
+++ src/handle_args.c
@@ -6,7 +6,7 @@
 /*   By: sjimenez <sjimenez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/27 07:47:30 by sjimenez          #+#    #+#             */
-/*   Updated: 2019/01/27 13:16:08 by sjimenez         ###   ########.fr       */
+/*   Updated: 2019/01/27 13:43:41 by sjimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,10 @@ int				handle_args(t_ssl *h, int ac, char **av)
 	if (!ft_strcmp(av[1], "md5") || !ft_strcmp(av[1], "sha256"))
 		h->algo_name = ft_strdup(av[1]);
 	else
+	{
 		print_message(3, NULL);
+		return (1);
+	}
 	if (!ft_strcmp(h->algo_name, "md5"))
 		h->algo = MD5;
 	else if (!ft_strcmp(h->algo_name, "sha256"))
@@ -80,26 +83,26 @@ int				handle_args(t_ssl *h, int ac, char **av)
 		i++;
 		j--;
 	}
-	h->file_path = (char**)ft_memalloc(sizeof(char*) * (h->file_ct + 1));
+	h->f_path = (char**)ft_memalloc(sizeof(char*) * (h->file_ct + 1));
 	while (++i < h->file_ct)
-		h->file_path[i] = ft_strdup(av[i + j + 1]);
-	h->file_txt = (char**)ft_memalloc(sizeof(char*) * (h->file_ct + 1));
+		h->f_path[i] = ft_strdup(av[i + j + 1]);
+	h->f_txt = (char**)ft_memalloc(sizeof(char*) * (h->file_ct + 1));
 	if (h->options & OPT_S)
 	{
 		if (!(h->options & OPT_P))
 		{
-			h->file_txt[0] = ft_strdup(h->file_path[0]);
-			h->file_path[0] = ft_strjoin_free("\"", h->file_path[0], 0);
-			h->file_path[0] = ft_strjoin_free(h->file_path[0], "\"", 0);
+			h->f_txt[0] = ft_strdup(h->f_path[0]);
+			h->f_path[0] = ft_strjoin_free("\"", h->f_path[0], 0);
+			h->f_path[0] = ft_strjoin_free(h->f_path[0], "\"", 0);
 		}
 		else
 		{
-			h->file_txt[1] = ft_strdup(h->file_path[1]);
-			h->file_path[1] = ft_strjoin_free("\"", h->file_path[1], 2);
-			h->file_path[1] = ft_strjoin_free(h->file_path[1], "\"", 1);
+			h->f_txt[1] = ft_strdup(h->f_path[1]);
+			h->f_path[1] = ft_strjoin_free("\"", h->f_path[1], 2);
+			h->f_path[1] = ft_strjoin_free(h->f_path[1], "\"", 1);
 		}
 	}
-	if (!h->algo_name || !h->file_path)
+	if (!h->algo_name || !h->f_path)
 		return (1);
 	return (0);
 }
